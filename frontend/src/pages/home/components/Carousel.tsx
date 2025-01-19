15a3cd7c3ce9176875cf416635280eac3001f647
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-19 23:13:49 +0530

    Add copyright header

--- frontend/src/pages/home/components/Carousel.tsx
+++ frontend/src/pages/home/components/Carousel.tsx
@@ -1,3 +1,10 @@
+/*!
+ * Bloodline Blood Bank Management System
+ * Copyright (c) 2025 Onaliy Jayawardana
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
 "use client";
 export function CarouselSlider() {
   return (
