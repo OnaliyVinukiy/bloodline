f0d40622c1bd7bc4bee339e2405388888fa27568
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-19 13:26:46 +0530

    Implement navigation bar

---
+++ frontend/src/components/common/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <AppBar position="fixed" sx={{ width: "100%" }}>
+      <Toolbar sx={{ paddingLeft: 0, paddingRight: 0 }}>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Bloodline
+        </Typography>
+
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/about">
+            About
+          </Button>
+          <Button color="inherit" component={Link} to="/services">
+            Services
+          </Button>
+          <Button color="inherit" component={Link} to="/contact">
+            Contact
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
