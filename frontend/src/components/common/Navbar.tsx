d6c9788034967fb05ac07ecfd4b68e0e41d38727
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-19 19:01:49 +0530

    Implement navigation bar

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,32 +1,44 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
-import { Link } from "react-router-dom";
+"use client";
 
-const Navbar = () => {
+import { Button, Navbar } from "flowbite-react";
+import bloodlogo from "../assets/bloodlogo.png";
+export function Navigationbar() {
   return (
-    <AppBar position="fixed" sx={{ width: "100%" }}>
-      <Toolbar sx={{ paddingLeft: 0, paddingRight: 0 }}>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+    <Navbar fluid rounded>
+      <Navbar.Brand
+        href="https://flowbite-react.com"
+        className="md:ml-24 h-6 sm:h-9"
+      >
+        <img
+          src="src/assets/bloodlogo.png"
+          className="mr-3 h-6 sm:h-9"
+          alt="Flowbite React Logo"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           Bloodline
-        </Typography>
-
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <Button color="inherit" component={Link} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/about">
-            About
-          </Button>
-          <Button color="inherit" component={Link} to="/services">
-            Services
-          </Button>
-          <Button color="inherit" component={Link} to="/contact">
-            Contact
-          </Button>
-        </Box>
-      </Toolbar>
-    </AppBar>
+        </span>
+      </Navbar.Brand>
+      <div className="flex md:order-2 md:mr-24">
+        <Button>Login</Button>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link href="#" className="text-lg font-medium" active>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#" className="text-lg font-medium">
+          About
+        </Navbar.Link>
+        <Navbar.Link href="#" className="text-lg font-medium">
+          Services
+        </Navbar.Link>
+        <Navbar.Link href="#" className="text-lg font-medium">
+          Appointments
+        </Navbar.Link>
+        <Navbar.Link href="#" className="text-lg font-medium">
+          Contact Us
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
   );
-};
-
-export default Navbar;
+}
