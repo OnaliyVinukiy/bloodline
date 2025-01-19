8917f93bc47849fdf78b4161e3232ce2376dbfe9
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-19 23:13:06 +0530

    Add gradiant to background image

--- frontend/src/pages/home/components/Carousel.tsx
+++ frontend/src/pages/home/components/Carousel.tsx
@@ -1,13 +1,10 @@
 "use client";
-
-import { Carousel } from "flowbite-react";
-
 export function CarouselSlider() {
   return (
     <section
       className="bg-cover bg-no-repeat bg-center h-[600px]"
       style={{
-        backgroundImage: `url('src/assets/carousel.jpg')`,
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('src/assets/carousel.jpg')`,
       }}
     >
       <div className="flex items-center h-full px-8">
@@ -17,11 +14,11 @@ export function CarouselSlider() {
             className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             role="alert"
           >
-            <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">
-              New
+            <span className="text-xs bg-red-600 rounded-full text-white px-4 py-1.5 mr-3">
+              Who we are
             </span>{" "}
             <span className="text-sm font-medium">
-              Flowbite is out! See what's new
+              Learn more about Bloodline!
             </span>
             <svg
               className="ml-2 w-5 h-5"
@@ -46,7 +43,7 @@ export function CarouselSlider() {
           <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
             <a
               href="#"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-red-800 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-red-800 hover:bg-red-700 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
             >
               Place an appointment
               <svg
@@ -64,7 +61,7 @@ export function CarouselSlider() {
             </a>
             <a
               href="#"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-300 hover:bg-black focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-black dark:focus:ring-gray-800"
             >
               <svg
                 className="mr-2 -ml-1 w-5 h-5"
