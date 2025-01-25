0d91b1c91a2b2996fb39b2f8dbc750a60d562a8a
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-01-25 13:36:38 +0530

    Add copyright header and add route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+/*!
+ * Bloodline Blood Bank Management System
+ * Copyright (c) 2025 Onaliy Jayawardana
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import { FooterComponent } from "./components/common/Footer";
 import { Navigationbar } from "./components/common/Navbar";
 import { Login } from "./pages/home/components/Login";
+import EligibilityCriteria  from "./pages/eligibility/Eligibility";
 import { useState } from "react";
 
 function App() {
@@ -27,6 +35,7 @@ function App() {
       />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/eligibility" element={<EligibilityCriteria />} />
       </Routes>
       <FooterComponent />
     </Router>
