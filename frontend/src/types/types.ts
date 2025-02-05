7eff281302143604181d2dd6f0f5843eb7734aa1
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-02-05 19:25:35 +0530

    Change type

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -3,7 +3,7 @@ export interface User {
     firstName: string;
     lastName: string;
     email: string;
-    birthdate: string | Date;
+    birthdate: Date | null;
     avatar: string | null;
     role: string;
   }
