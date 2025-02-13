16e1958808f19f7e27b46ccee22bebd1ef3a6166
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-02-13 21:46:01 +0530

    Add loading animation

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -11,35 +11,34 @@ import { useAuthContext } from "@asgardeo/auth-react";
 import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
 import { UserIcon } from "@heroicons/react/24/solid";
 import { User } from "../../types/types";
+import axios from "axios";
 
 export function Navigationbar() {
   const { state, signIn, signOut, getAccessToken } = useAuthContext();
   const [user, setUser] = useState<User | null>(null);
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
   // Fetch and assign user info
   useEffect(() => {
     const fetchUserInfo = async () => {
       if (state?.isAuthenticated) {
         try {
+          setIsLoading(true);
           const accessToken = await getAccessToken();
-          const response = await fetch("http://localhost:5000/api/user-info", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ accessToken }),
-          });
 
-          if (response.ok) {
-            const userInfo = await response.json();
-            setUser(userInfo);
-            console.log("user", userInfo);
-          } else {
-            throw new Error("Failed to fetch user info");
-          }
+          const response = await axios.post(
+            "http://localhost:5000/api/user-info",
+            { accessToken },
+            { headers: { "Content-Type": "application/json" } }
+          );
+
+          setUser(response.data);
+          console.log("user", response.data);
         } catch (error) {
           console.error("Error fetching user info:", error);
+        } finally {
+          setIsLoading(false);
         }
       } else {
         setUser(null);
@@ -48,6 +47,29 @@ export function Navigationbar() {
 
     fetchUserInfo();
   }, [state?.isAuthenticated, getAccessToken]);
+
+  if (isLoading) {
+    return (
+      <div className="loading flex justify-center items-center h-screen">
+        <svg width="64px" height="48px">
+          <polyline
+            points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"
+            id="back"
+            stroke="#e53e3e"
+            strokeWidth="2"
+            fill="none"
+          ></polyline>
+          <polyline
+            points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"
+            id="front"
+            stroke="#f56565"
+            strokeWidth="2"
+            fill="none"
+          ></polyline>
+        </svg>
+      </div>
+    );
+  }
 
   return (
     <Navbar fluid rounded>
