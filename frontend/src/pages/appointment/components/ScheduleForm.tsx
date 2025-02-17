17cce1b008d421f62476f0dae4f2dd75fef2b66d
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-02-17 11:33:57 +0530

    Add banner

--- frontend/src/pages/appointment/components/ScheduleForm.tsx
+++ frontend/src/pages/appointment/components/ScheduleForm.tsx
@@ -38,15 +38,16 @@ const ScheduleForm: React.FC<StepperProps> = ({
       <main className="mt-2 mb-16 flex justify-center items-center w-full max-w-4xl px-4 py-6 md:w-2/3 lg:w-3/4">
         <div className="w-full max-w-4xl bg-white p-6 rounded-lg shadow-lg">
           <div className="mt-4 space-y-6">
+            <div className="mb-6 p-4 bg-yellow-100 rounded-lg">
+              <p className="text-yellow-800 font-opensans font-semibold">
+                Pick a date and time slot for the blood donation
+              </p>
+            </div>
             <div className="flex flex-col items-center p-6">
-              <h1 className="text-2xl font-bold text-indigo-900 mb-4">
-                Select Date & Time for Appointment
-              </h1>
-
               <div className="mb-6">
                 <Label
                   htmlFor="date"
-                  className="block mb-2 text-sm font-medium"
+                  className="block mb-2 text-md font-semibold font-opensans"
                 >
                   Select a Date
                 </Label>
@@ -63,20 +64,24 @@ const ScheduleForm: React.FC<StepperProps> = ({
               </div>
 
               {selectedDate && (
-                <div className="mb-6">
+                <div>
                   <Label
                     htmlFor="time-slots"
-                    className="block mb-2 text-sm font-medium"
+                    className="block mb-2 text-md font-semibold font-opensans"
                   >
                     Select an Available Time Slot
                   </Label>
-                  <div className="flex flex-wrap gap-4">
+                  <div className="mt-4 flex flex-wrap gap-5">
                     {timeSlots.map((slot, index) => (
                       <Button
                         key={index}
                         onClick={() => setSelectedSlot(slot)}
                         color={selectedSlot === slot ? "success" : "light"}
                       >
+                        <svg className="w-5 h-5 text-black me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+</svg>
+
                         {slot}
                       </Button>
                     ))}
@@ -84,11 +89,16 @@ const ScheduleForm: React.FC<StepperProps> = ({
                 </div>
               )}
             </div>
-            <div className="flex justify-end mt-6">
+            <div className="flex justify-between">
+              <button
+                onClick={onPreviousStep}
+                className="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2 dark:hover:text-white "
+              >
+                Back
+              </button>
               <button
                 onClick={onNextStep}
-                className="px-4 py-2 text-white bg-red-800 rounded-lg hover:bg-red-700"
-                disabled={!selectedDate || !selectedSlot}
+                className="focus:outline-none text-white bg-red-800 hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
               >
                 Next
               </button>
