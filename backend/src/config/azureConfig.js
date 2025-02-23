75a99cdf7c7cc0bdec53fd962e1ab778f755edef
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-02-23 12:00:11 +0530

    feat: Implement azure configuration

---
+++ backend/src/config/azureConfig.js
@@ -0,0 +1,18 @@
+/*!
+ * Bloodline Blood Bank Management System
+ * Copyright (c) 2025 Onaliy Jayawardana
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const COSMOS_DB_CONNECTION_STRING = process.env.COSMOS_DB_CONNECTION_STRING;
+export const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
+export const DATABASE_ID = "donorDB";
+export const APPOINTMENT_COLLECTION_ID = "appointments";
+export const DONOR_COLLECTION_ID = "donors";
+export const CONTAINER_NAME = "profile-pictures";
