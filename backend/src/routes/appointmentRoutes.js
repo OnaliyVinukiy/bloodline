ae020fae966c6a8923d314d57aa99ab844cc9cb1
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-02-24 21:11:36 +0530

    feat: Register routes

--- backend/src/routes/appointmentRoutes.js
+++ backend/src/routes/appointmentRoutes.js
@@ -6,11 +6,19 @@
  * Unauthorized copying, modification, or distribution of this code is prohibited.
  */
 import express from "express";
-import { saveAppointment } from "../controllers/appointmentController.js";
+import {
+  saveAppointment,
+  getAppointments,
+  getAppointmentsByDate,
+} from "../controllers/appointmentController.js";
 
 const router = express.Router();
 
 // Route to save appointment data
 router.post("/save-appointment", saveAppointment);
+
+//Routes to fetch appointment data
+router.get("/fetch-appointment", getAppointments);
+router.get("/:date", getAppointmentsByDate);
 
 export default router;
