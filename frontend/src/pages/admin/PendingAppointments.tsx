49941ba836a4efe6367b4bd229e1e1c46fd3df71
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-02-27 13:28:56 +0530

    feat: Display additional features in pending appoinments list

--- frontend/src/pages/admin/PendingAppointments.tsx
+++ frontend/src/pages/admin/PendingAppointments.tsx
@@ -1,9 +1,19 @@
+/*!
+ * Bloodline Blood Bank Management System
+ * Copyright (c) 2025 Onaliy Jayawardana
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const PendingAppointments = () => {
   const [appointments, setAppointments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
+  //Fetch pending appointments
   useEffect(() => {
     const fetchAppointments = async () => {
       try {
@@ -14,17 +24,44 @@ const PendingAppointments = () => {
           (appointment: any) => appointment.status === "pending"
         );
         setAppointments(pendingAppointments);
+        console.log(pendingAppointments);
       } catch (error) {
         console.error("Error fetching appointments:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchAppointments();
   }, []);
 
+  //Loading animation
+  if (isLoading) {
+    return (
+      <div className="loading flex justify-center items-center h-screen">
+        <svg width="64px" height="48px">
+          <polyline
+            points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"
+            id="back"
+            stroke="#e53e3e"
+            strokeWidth="2"
+            fill="none"
+          ></polyline>
+          <polyline
+            points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"
+            id="front"
+            stroke="#f56565"
+            strokeWidth="2"
+            fill="none"
+          ></polyline>
+        </svg>
+      </div>
+    );
+  }
+
   return (
     <div className="flex justify-center mt-8">
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-w-6xl w-full">
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-w-7xl w-full mb-20">
         <div className="mt-4 ml-4 flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
           <label htmlFor="table-search" className="sr-only">
             Search
@@ -55,11 +92,20 @@ const PendingAppointments = () => {
             />
           </div>
         </div>
-        <table className="mt-4 mb-4 w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table className="mt-4 mb-4 w-full text-md text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text- text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
                 Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Date
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Time
               </th>
               <th scope="col" className="px-6 py-3">
                 NIC
@@ -71,7 +117,7 @@ const PendingAppointments = () => {
                 Status
               </th>
 
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-6 py-3 text-center">
                 Action
               </th>
               <th scope="col" className="px-1 py-1"></th>
@@ -83,96 +129,117 @@ const PendingAppointments = () => {
                 key={appointment._id}
                 className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
               >
-                <td className="px-6 py-4">{appointment.donorInfo.fullName}</td>
-                <td className="px-6 py-4">{appointment.donorInfo.nic}</td>
                 <td className="px-6 py-4">
+                  <img
+                    className="w-10 h-10 rounded-full"
+                    src={appointment.donorInfo.avatar}
+                    alt="Jese image"
+                  />
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {appointment.donorInfo.fullName}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {appointment.selectedDate}
+                </td>
+                <td className="px-6 py-4">{appointment.selectedSlot}</td>
+
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {appointment.donorInfo.nic}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
                   {appointment.donorInfo.contactNumber}
                 </td>
-                <td className="px-6 py-4 flex items-center">
+                <td className="px-6 py-6 flex items-center whitespace-nowrap">
                   <div className="h-2.5 w-2.5 rounded-full bg-yellow-500 mr-2"></div>
                   {appointment.status.charAt(0).toUpperCase() +
                     appointment.status.slice(1)}
                 </td>
 
-                <td className="px-8 py-4">
-                  <div className="flex space-x-4">
-                    <button
-                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      aria-label="View"
-                    >
-                      <svg
-                        className="w-6 h-6 text-gray-800 dark:text-white"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
+                <td className="px-8 py-4 text-center">
+                  <div className="flex justify-center space-x-4">
+                    <Link to={`/appointment/${appointment._id}`}>
+                      <button
+                        className="font-medium text-yellow-400 dark:text-yellow-500 hover:underline"
+                        aria-label="View"
                       >
-                        <path
-                          fillRule="evenodd"
-                          d="M4.998 7.78C6.729 6.345 9.198 5 12 5c2.802 0 5.27 1.345 7.002 2.78a12.713 12.713 0 0 1 2.096 2.183c.253.344.465.682.618.997.14.286.284.658.284 1.04s-.145.754-.284 1.04a6.6 6.6 0 0 1-.618.997 12.712 12.712 0 0 1-2.096 2.183C17.271 17.655 14.802 19 12 19c-2.802 0-5.27-1.345-7.002-2.78a12.712 12.712 0 0 1-2.096-2.183 6.6 6.6 0 0 1-.618-.997C2.144 12.754 2 12.382 2 12s.145-.754.284-1.04c.153-.315.365-.653.618-.997A12.714 12.714 0 0 1 4.998 7.78ZM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </button>
-
-                    <button
-                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      aria-label="Approve"
-                    >
-                      <svg
-                        className="w-6 h-6 text-gray-800 dark:text-white"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
+                        <svg
+                          className="w-6 h-6 text-yellow-400 dark:text-white"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="currentColor"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M4.998 7.78C6.729 6.345 9.198 5 12 5c2.802 0 5.27 1.345 7.002 2.78a12.713 12.713 0 0 1 2.096 2.183c.253.344.465.682.618.997.14.286.284.658.284 1.04s-.145.754-.284 1.04a6.6 6.6 0 0 1-.618.997 12.712 12.712 0 0 1-2.096 2.183C17.271 17.655 14.802 19 12 19c-2.802 0-5.27-1.345-7.002-2.78a12.712 12.712 0 0 1-2.096-2.183 6.6 6.6 0 0 1-.618-.997C2.144 12.754 2 12.382 2 12s.145-.754.284-1.04c.153-.315.365-.653.618-.997A12.714 12.714 0 0 1 4.998 7.78ZM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </button>
+                    </Link>
+                    <Link to={`/appointment/${appointment._id}`}>
+                      <button
+                        className="font-medium text-green-600 dark:text-green-500 hover:underline"
+                        aria-label="Approve"
                       >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M5 11.917L9.724 16.5 19 7.5"
-                        />
-                      </svg>
-                    </button>
-                    <button
-                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      aria-label="Approve"
-                    >
-                      <svg
-                        className="w-6 h-6 text-gray-800 dark:text-white"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="none"
-                        viewBox="0 0 24 24"
+                        <svg
+                          className="w-6 h-6 text-green-600 dark:text-white"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 11.917L9.724 16.5 19 7.5"
+                          />
+                        </svg>
+                      </button>
+                    </Link>
+                    <Link to={`/appointment/${appointment._id}`}>
+                      <button
+                        className="font-medium text-red-600 dark:text-red-500 hover:underline"
+                        aria-label="Reject"
                       >
-                        <path
-                          stroke="currentColor"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M6 18 17.94 6M18 18 6.06 6"
-                        />
-                      </svg>
-                    </button>
+                        <svg
+                          className="w-6 h-6 text-red-600 dark:text-white"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18 17.94 6M18 18 6.06 6"
+                          />
+                        </svg>
+                      </button>
+                    </Link>
                   </div>
                 </td>
 
                 <td className="px-1 py-1">
-                  {(appointment.fourthform?.isMedicallyAdvised === "yes" ||
-                    appointment.secondForm?.isPregnant === "yes") && (
+                  {(appointment.fourthform?.isMedicallyAdvised === "Yes" ||
+                    appointment.secondForm?.isPregnant === "Yes") && (
                     <button
-                      className="text-yellow-500 hover:text-yellow-400 ml-2"
+                      className="text-red-500 hover:text-red-400 ml-2"
                       aria-label="Warning"
                     >
                       <svg
-                        className="w-6 h-6 text-gray-800 dark:text-white"
+                        className="w-6 h-6 text-red-500"
                         aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
