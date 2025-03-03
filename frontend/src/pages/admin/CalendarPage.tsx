f5a642f43a155a4585ad0c8181e4276d9562ab97
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-03 19:10:26 +0530

    feat: Move calendar tab to a separate page

--- frontend/src/pages/admin/CalendarPage.tsx
+++ frontend/src/pages/admin/CalendarPage.tsx
@@ -5,143 +5,46 @@
  *
  * Unauthorized copying, modification, or distribution of this code is prohibited.
  */
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import React, { useState } from "react";
+import PendingAppointments from "./PendingAppointments";
+import AppointmentCalendar from "./AppointmentsCalendar";
 
 const CalendarPage = () => {
-  const [appointments, setAppointments] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [selectedDate, setSelectedDate] = useState(new Date());
-
-  //Fetch appointments
-  useEffect(() => {
-    const fetchAppointments = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/appointments/fetch-appointment"
-        );
-        setAppointments(response.data);
-      } catch (error) {
-        console.error("Error fetching appointments:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchAppointments();
-  }, []);
-
-  // Filter appointments by selected date
-  const filteredAppointments = appointments.filter(
-    (appointment: any) =>
-      new Date(appointment.selectedDate).toDateString() ===
-      selectedDate.toDateString()
-  );
-
-  //Loading animation
-  if (isLoading) {
-    return (
-      <div className="loading flex justify-center items-center h-screen">
-        <svg width="64px" height="48px">
-          <polyline
-            points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"
-            id="back"
-            stroke="#e53e3e"
-            strokeWidth="2"
-            fill="none"
-          ></polyline>
-          <polyline
-            points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24"
-            id="front"
-            stroke="#f56565"
-            strokeWidth="2"
-            fill="none"
-          ></polyline>
-        </svg>
-      </div>
-    );
-  }
-
+  const [activeTab, setActiveTab] = useState("tab1");
   return (
-    <div className="flex flex-col items-center mt-8">
-       <Calendar
-          onChange={(date) => setSelectedDate(date as Date)}
-          value={selectedDate}
-          className="custom-calendar"
-          tileClassName="text-center p-2 rounded-md transition duration-200 hover:bg-blue-500 hover:text-white"
-        />
+    <div className="mt-12">
+      <div className="flex justify-center border-b border-gray-200 dark:border-gray-700">
+        <ul className="flex flex-wrap -mb-px text-sm font-medium text-center">
+          <li className="me-2">
+            <button
+              onClick={() => setActiveTab("tab1")}
+              className={`px-6 py-3 rounded-t-lg transition-colors duration-300 ${
+                activeTab === "tab1"
+                  ? "text-white bg-red-800 border-b-0"
+                  : "text-gray-600 dark:text-gray-400 hover:text-red-600 hover:bg-gray-100 dark:hover:bg-gray-800"
+              }`}
+            >
+              Appointments
+            </button>
+          </li>
+          <li className="me-2">
+            <button
+              onClick={() => setActiveTab("tab2")}
+              className={`px-6 py-3 rounded-t-lg transition-colors duration-300 ${
+                activeTab === "tab2"
+                  ? "text-white bg-red-800 border-b-0"
+                  : "text-gray-600 dark:text-gray-400 hover:text-red-600 hover:bg-gray-100 dark:hover:bg-gray-800"
+              }`}
+            >
+              Campaigns
+            </button>
+          </li>
+        </ul>
+      </div>
 
-      <div className="mt-8 relative overflow-x-auto shadow-md sm:rounded-lg max-w-7xl w-full mb-20">
-        <h2 className="text-lg font-semibold text-center mb-4">
-          Appointments for {selectedDate.toDateString()}
-        </h2>
-        <table className="mt-4 mb-4 w-full text-md text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text- text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Time
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                NIC
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Contact No
-              </th>
-              <th scope="col" className="px-6 py-3 text-center">
-                Status
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredAppointments.map((appointment: any) => (
-              <tr
-                key={appointment._id}
-                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
-              >
-                <td className="px-6 py-4">{appointment.selectedSlot}</td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  {appointment.donorInfo.fullName}
-                </td>
-
-                <td className="px-6 py-4">{appointment.donorInfo.nic}</td>
-                <td className="px-6 py-4">
-                  {appointment.donorInfo.contactNumber}
-                </td>
-                {appointment.status === "Rejected" ? (
-                  <td className="px-6 py-6 text-center">
-                    <div className="badges flex justify-center">
-                      <button className="red">Rejected</button>
-                    </div>
-                  </td>
-                ) : appointment.status === "Approved" ? (
-                  <td className="px-6 py-6 text-center">
-                    <div className="badges flex justify-center">
-                      <button className="green">Approved</button>
-                    </div>
-                  </td>
-                ) : (
-                  <td className="px-6 py-6 text-center">
-                    <div className="badges flex justify-center">
-                      <button className="yellow">Pending</button>
-                    </div>
-                  </td>
-                )}
-              </tr>
-            ))}
-            {filteredAppointments.length === 0 && (
-              <tr>
-                <td colSpan={5} className="text-center px-6 py-4 text-gray-500">
-                  No appointments found.
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+      <div>
+        {activeTab === "tab1" && <AppointmentCalendar />}
+        {activeTab === "tab2" && <PendingAppointments />}
       </div>
     </div>
   );
