fbeede7082de35ca66f4f094d4ee3713e022978d
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-04 16:02:34 +0530

    style: Add transition to buttons

--- frontend/src/pages/appointment/components/StepEight.tsx
+++ frontend/src/pages/appointment/components/StepEight.tsx
@@ -248,7 +248,7 @@ const StepEight: React.FC<StepperProps> = ({
             <div className="flex justify-between mt-6">
               <button
                 onClick={onPreviousStep}
-                className="text-red-800 hover:text-white border border-red-800 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                className="text-red-800 hover:text-white border border-red-800 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition-all duration-300"
               >
                 Back
               </button>
@@ -260,7 +260,7 @@ const StepEight: React.FC<StepperProps> = ({
               <button
                 onClick={submitForm}
                 disabled={loading}
-                className="focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-red-800 hover:bg-red-700 focus:ring-4 focus:ring-red-300 disabled:bg-red-500 disabled:cursor-not-allowed flex items-center justify-center"
+                className="focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-red-800 hover:bg-red-700 focus:ring-4 focus:ring-red-300 disabled:bg-red-500 disabled:cursor-not-allowed flex items-center justify-center transition-all duration-300"
               >
                 {loading ? (
                   <>
