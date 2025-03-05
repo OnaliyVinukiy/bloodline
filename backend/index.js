dfe9e84d1bd6cd5c64c6dcb72da7bfae1a3dd3fc
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-05 20:46:48 +0530

    Fix CORS issue

--- backend/index.js
+++ backend/index.js
@@ -11,20 +11,18 @@ import userRoutes from "./src/routes/userRoutes.js";
 import appointmentRoutes from "./src/routes/appointmentRoutes.js";
 
 const app = express();
-const allowedOrigins = [
-  "https://bloodline-gxfvfrfyg7bqbahz.southeastasia-01.azurewebsites.net",
-  "https://bloodlinebackend-avepf5h9fdfsera7.southeastasia-01.azurewebsites.net",
-];
+
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: "*",
     methods: ["GET", "POST", "PUT", "OPTIONS", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
 app.use(express.json());
+app.options("*", cors());
 
 //User Routes
 app.use("/api", userRoutes);
@@ -35,4 +33,12 @@ app.use("/api/appointments", appointmentRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  try {
+    res.send("API Working...!");
+  } catch (error) {
+    res.json({ error });
+  }
 });
