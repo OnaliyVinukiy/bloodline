c1566d19a4060dded6fb59d6fbe4611e1b79a767
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-07 19:18:07 +0530

    refactor: Minor code refactor

--- frontend/src/pages/campaign/components/StepThree.tsx
+++ frontend/src/pages/campaign/components/StepThree.tsx
@@ -75,7 +75,7 @@ const StepThree: React.FC<StepperPropsCampaign> = ({
                 onClick={handleNext}
                 className="focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-red-800 hover:bg-red-700 focus:ring-4 focus:ring-red-300 transition-all duration-300"
               >
-                Next.
+                Next
               </button>
             </div>
           </div>
