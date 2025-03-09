c1532cf0553b2a0e241f9d24b5c96a002868db8c
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-09 19:54:45 +0530

    feat: Implement email template for appointment confirmation

---
+++ backend/src/emailTemplates/AppointmentConfirmation.ts
@@ -0,0 +1,28 @@
+/*!
+ * Bloodline Blood Bank Management System
+ * Copyright (c) 2025 Onaliy Jayawardana
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
+export const AppointmentConfirmation = (appointment: any) => {
+  return `
+    <div style="font-family: Arial, sans-serif; background-color: #f4f4f9; padding: 20px; border-radius: 10px; max-width: 600px; margin: 0 auto;">
+      <div style="background-color: #e2e8f0; padding: 10px; border-radius: 5px; text-align: center;">
+        <h2 style="color: #2d3748;">Appointment Confirmation</h2>
+      </div>
+      <div style="margin-top: 20px;">
+        <p style="font-size: 16px; color: #4a5568;">Dear ${appointment.donorInfo.fullName},</p>
+        <p style="font-size: 16px; color: #4a5568;">Your appointment request has been placed successfully. The NBTS team will review the application and will send the acceptance or rejection status.</p>
+        <p style="font-size: 16px; color: #4a5568;"><strong>Appointment Date:</strong> ${appointment.selectedDate}</p>
+         <p style="font-size: 16px; color: #4a5568;"><strong>Appointment Time:</strong> ${appointment.selectedSlot}</p>
+           <p style="font-size: 16px; color: #4a5568;"><strong>Appointment Status:</strong> ${appointment.status}</p>
+        <p style="font-size: 16px; color: #4a5568;">Thank you for your contribution to saving lives!</p>
+      </div>
+      <div style="margin-top: 30px; text-align: center; font-size: 16px; color: #4a5568;">
+        <p>Best regards,</p>
+        <p><strong>NBTS</strong></p>
+      </div>
+    </div>
+  `;
+};
