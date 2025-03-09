2acc8597cfc387511cea6f02e95d0dc5b16edf30
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-09 20:24:31 +0530

    feat: Implement inteface for user info

---
+++ frontend/src/types/users.ts
@@ -0,0 +1,49 @@
+/*!
+ * Bloodline Blood Bank Management System
+ * Copyright (c) 2025 Onaliy Jayawardana
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
+import { ObjectId } from "mongodb";
+
+export interface User {
+  sub: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthdate: Date | null;
+  avatar: string | null;
+  role: string;
+}
+
+export interface Donor {
+  _id?: ObjectId;
+  nic: string;
+  fullName: string;
+  email: string;
+  contactNumber: string;
+  address: string;
+  birthdate: string;
+  age: number;
+  bloodGroup: string;
+  avatar: string | null;
+  gender: string;
+}
+
+export interface BloodDonor {
+  _id?: ObjectId;
+  nic: string;
+  fullName: string;
+  email: string;
+  contactNumber: string;
+  contactNumberHome: string;
+  contactNumberOffice: string;
+  address: string;
+  addressOffice: string;
+  birthdate: string;
+  age: number;
+  bloodGroup: string;
+  avatar: string | null;
+  gender: string;
+}
