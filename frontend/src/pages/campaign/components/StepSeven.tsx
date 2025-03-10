20244b12ca889a79ca35ef5e1296f49deb0f3a6c
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-10 20:52:47 +0530

    style: Change styles of the stepper components

--- frontend/src/pages/campaign/components/StepSeven.tsx
+++ frontend/src/pages/campaign/components/StepSeven.tsx
@@ -18,82 +18,77 @@ const StepSeven: React.FC<StepperPropsCampaign> = ({
   };
 
   return (
-    <div className="flex justify-center bg-gradient-to-r from-gray-50 to-gray-100">
+    <div className="flex justify-center bg-gradient-to-r from-gray-50 to-gray-100 min-h-screen">
       <main className="mt-8 mb-8 w-full max-w-4xl px-4 py-8">
         <div className="relative bg-white rounded-xl shadow-2xl overflow-hidden p-1">
           <div className="absolute inset-0 bg-gradient-to-r from-red-600 to-red-800 rounded-xl z-0"></div>
 
           <div className="relative bg-white rounded-xl p-8 z-10">
             <div className="text-center mb-8">
-              <h2 className="mt-4 flex justify-center items-center gap-4 text-2xl md:text-3xl font-bold text-gray-800">
+              <div className="flex flex-col items-center gap-4">
                 <svg
-                  className="w-10 h-10 text-gray-800"
+                  className="w-12 h-12 text-red-700"
                   aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  viewBox="0 0 448 512"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
                     stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M416 0C400 0 288 32 288 176V288c0 35.3 28.7 64 64 64h32V480c0 17.7 14.3 32 32 32s32-14.3 32-32V352 240 32c0-17.7-14.3-32-32-32zM64 16C64 7.8 57.9 1 49.7 .1S34.2 4.6 32.4 12.5L2.1 148.8C.7 155.1 0 161.5 0 167.9c0 45.9 35.1 83.6 80 87.7V480c0 17.7 14.3 32 32 32s32-14.3 32-32V255.6c44.9-4.1 80-41.8 80-87.7c0-6.4-.7-12.8-2.1-19.1L191.6 12.5c-1.8-8-9.3-13.3-17.4-12.4S160 7.8 160 16V150.2c0 5.4-4.4 9.8-9.8 9.8c-5.1 0-9.3-3.9-9.8-9L127.9 14.6C127.2 6.3 120.3 0 112 0s-15.2 6.3-15.9 14.6L83.7 151c-.5 5.1-4.7 9-9.8 9c-5.4 0-9.8-4.4-9.8-9.8V16zm48.3 152l-.3 0-.3 0 .3-.7 .3 .7z"
                   />
                 </svg>
-                Equipments & Assets Required
-              </h2>
-              <div className="mt-2 text-base md:text-lg text-gray-500">
+                <h2 className="text-3xl md:text-4xl font-bold text-gray-800">
+                  Food and Beverages for Donors
+                </h2>
+              </div>
+              <div className="mt-2 text-lg md:text-xl text-gray-600">
                 Every drop counts. Let’s make a difference together!
               </div>
             </div>
 
-            <div className="bg-gray-50 p-6 rounded-lg border border-gray-100 shadow-sm">
-              <div className="mt-4 mb-8 font-roboto font-medium text-base md:text-lg text-justify text-gray-700 leading-relaxed">
-                To ensure the safety and well-being of both donors and collected
-                blood, the National Blood Transfusion Service (NBTS) Sri Lanka
-                does not approve the use of temporary buildings like tents. The
-                selected venue must meet the following essential requirements:
-                <ul className="list-disc pl-6 mt-4 space-y-2">
+            <div className="bg-gray-50 p-8 rounded-lg border border-gray-100 shadow-sm">
+              <div className="mt-4 mb-8 font-roboto font-medium text-md md:text-xl text-justify text-gray-700 leading-relaxed">
+                When organizing a blood donation campaign for{" "}
+                <strong className="text-red-700">
+                  approximately 100 donors,
+                </strong>{" "}
+                the following equipment should be arranged to ensure a smooth
+                and comfortable process:
+                <ul className="pl-6 mt-6 space-y-4">
                   <li>
-                    <strong>Permanent Building</strong> – The facility should be
-                    a solid, secure structure to provide a stable and safe
-                    environment.
+                    <ul className="list-[square] pl-6 mt-2 space-y-2">
+                      <li>5 office tables</li>
+                      <li>15 normal chairs</li>
+                      <li>
+                        Chairs or benches for donors to sit (Must accommodate at
+                        least 30 donors at once while waiting or resting after
+                        donation)
+                      </li>
+                    </ul>
+                  </li>
+
+                  <li>
+                    <ul className="list-[square] pl-6 mt-2 space-y-2">
+                      <li>7 fans</li>
+                    </ul>
                   </li>
                   <li>
-                    <strong>No Water Leakages</strong> – he building must be
-                    free from leaks to maintain a hygienic space for the blood
-                    donation process.
-                  </li>
-                  <li>
-                    <strong>Clean, Dust-Free Floor</strong> – A flat and
-                    dust-free ground ensures a sanitary and comfortable
-                    experience for donors and medical staff.
-                  </li>
-                  <li>
-                    <strong>Proper Lighting & Ventilation</strong> – Sufficient
-                    lighting and air circulation are necessary for a comfortable
-                    and safe atmosphere.
-                  </li>
-                  <li>
-                    <strong>Electricity & Water Facilities</strong> – The venue
-                    must have a reliable electricity supply and access to clean
-                    water.
-                  </li>
-                  <li>
-                    <strong>Easy Accessibility for Donors</strong> – If the
-                    venue is on an upper floor, an elevator must be available
-                    for donors. If no elevator is present, the venue should be
-                    on the ground floor to avoid the need for stair access.
+                    <ul className="list-[square] pl-6 mt-2 space-y-2">
+                      <li>
+                        All necessary medical supplies, beds and other equipment
+                        required for the blood collection process will be
+                        provided by the National Blood Transfusion Service
+                        (NBTS) or the nearest blood bank.
+                      </li>
+                    </ul>
                   </li>
                 </ul>
-                <br />
-                Before finalizing the venue, organizers must discuss with the
-                NBTS or the nearest blood bank to confirm that the space meets
-                all necessary requirements. A well-prepared venue ensures a
-                smooth and successful blood donation drive!
               </div>
             </div>
 
