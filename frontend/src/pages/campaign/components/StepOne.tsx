2f83971f443296636aff52a239e215df64da965d
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-11 13:02:09 +0530

    refactor: Change campaign to camp

--- frontend/src/pages/campaign/components/StepOne.tsx
+++ frontend/src/pages/campaign/components/StepOne.tsx
@@ -25,10 +25,10 @@ const StepOne: React.FC<StepperPropsCampaign> = ({ onNextStep }) => {
           <div className="relative bg-white rounded-xl p-8 z-10">
             <div className="text-center mb-8">
               <h2 className="mt-4 text-2xl md:text-4xl font-bold text-gray-800">
-                Organize a Blood Donation Campaign
+                Organize a Blood Donation Camp
               </h2>
               <div className="mt-2 text-lg md:text-xl text-gray-600">
-                Together, we can save lives. Start your campaign today!
+                Together, we can save lives. Start your camp today!
               </div>
             </div>
 
@@ -43,10 +43,10 @@ const StepOne: React.FC<StepperPropsCampaign> = ({ onNextStep }) => {
             <div className="bg-gray-50 p-8 rounded-lg border border-gray-100 shadow-sm">
               <div className="mt-4 mb-8 font-roboto font-medium text-md md:text-lg text-center text-gray-700 leading-relaxed">
                 Our main goal is to encourage and support blood donation to help
-                save lives. You can organize a blood donation campaign for any
+                save lives. You can organize a blood donation camp for any
                 reason, whether it's for a national or religious event or even a
                 personal occasion like your birthday. You can choose to hold the
-                campaign in your own community or at one of our blood centers.
+                camp in your own community or at one of our blood centers.
                 If you decide to host it at one of our blood centers, our team
                 will be there to support you every step of the way. Together, we
                 can make a difference!
@@ -56,7 +56,7 @@ const StepOne: React.FC<StepperPropsCampaign> = ({ onNextStep }) => {
             <div className="flex justify-end mt-8">
               <button
                 onClick={handleNext}
-                className="focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-red-800 hover:bg-red-700 focus:ring-4 focus:ring-red-300 transition-all duration-300"
+                className="focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-red-800 hover:bg-red-700  focus:ring-4 focus:ring-red-300 transition-all duration-300"
               >
                 Next
               </button>
