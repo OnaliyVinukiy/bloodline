a98b7a7114bd42cf7733876cbe5a9c44b71eadff
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-12 23:52:48 +0530

    feat: Add props to get date and time

--- frontend/src/pages/campaign/components/StepTwelve.tsx
+++ frontend/src/pages/campaign/components/StepTwelve.tsx
@@ -12,10 +12,12 @@ import { Label } from "flowbite-react";
 import axios from "axios";
 import { Camp } from "../../../types/camp";
 
-const StepTwelve: React.FC<StepperPropsCampaign> = ({
-  onNextStep,
-  onPreviousStep,
-}) => {
+const StepTwelve: React.FC<
+  StepperPropsCampaign & {
+    selectedDate: Date | null;
+    selectedSlot: string | null;
+  }
+> = ({ onNextStep, onPreviousStep, selectedDate, selectedSlot }) => {
   const handlePrevious = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
     onPreviousStep();
@@ -47,7 +49,12 @@ const StepTwelve: React.FC<StepperPropsCampaign> = ({
     province: "",
     district: "",
     city: "",
+    date: selectedDate ? selectedDate.toISOString().split("T")[0] : "",
+    time: selectedSlot || "",
   });
+  useEffect(() => {
+    console.log(formData);
+  }, []);
 
   const [errors, setErrors] = useState<{ [key in keyof Camp]?: string }>({});
 
@@ -168,7 +175,7 @@ const StepTwelve: React.FC<StepperPropsCampaign> = ({
                   width="24"
                   height="24"
                   fill="currentColor"
-                  viewBox="0 0 576 512" 
+                  viewBox="0 0 576 512"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
@@ -176,7 +183,7 @@ const StepTwelve: React.FC<StepperPropsCampaign> = ({
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                   d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V285.7l-86.8 86.8c-10.3 10.3-17.5 23.1-21 37.2l-18.7 74.9c-2.3 9.2-1.8 18.8 1.3 27.5H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zM549.8 235.7l14.4 14.4c15.6 15.6 15.6 40.9 0 56.6l-29.4 29.4-71-71 29.4-29.4c15.6-15.6 40.9-15.6 56.6 0zM311.9 417L441.1 287.8l71 71L382.9 487.9c-4.1 4.1-9.2 7-14.9 8.4l-60.1 15c-5.5 1.4-11.2-.2-15.2-4.2s-5.6-9.7-4.2-15.2l15-60.1c1.4-5.6 4.3-10.8 8.4-14.9z"
+                    d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V285.7l-86.8 86.8c-10.3 10.3-17.5 23.1-21 37.2l-18.7 74.9c-2.3 9.2-1.8 18.8 1.3 27.5H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zM549.8 235.7l14.4 14.4c15.6 15.6 15.6 40.9 0 56.6l-29.4 29.4-71-71 29.4-29.4c15.6-15.6 40.9-15.6 56.6 0zM311.9 417L441.1 287.8l71 71L382.9 487.9c-4.1 4.1-9.2 7-14.9 8.4l-60.1 15c-5.5 1.4-11.2-.2-15.2-4.2s-5.6-9.7-4.2-15.2l15-60.1c1.4-5.6 4.3-10.8 8.4-14.9z"
                   />
                 </svg>
                 <h2 className="text-2xl md:text-4xl font-bold text-gray-800">
@@ -280,7 +287,9 @@ const StepTwelve: React.FC<StepperPropsCampaign> = ({
                     required
                   />
                   {errors.contactNumber && (
-                    <p className="text-red-500 text-xs mt-1">{errors.contactNumber}</p>
+                    <p className="text-red-500 text-xs mt-1">
+                      {errors.contactNumber}
+                    </p>
                   )}
                 </div>
                 <div className="flex flex-col md:flex-row md:space-x-6 space-y-4 md:space-y-0 mb-6">
