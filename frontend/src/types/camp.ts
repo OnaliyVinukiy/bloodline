5d9d86b7df51d5dade8a6fe633f84e2fd5234fbc
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-12 23:17:01 +0530

    feat: Implement interface for donation camp

---
+++ frontend/src/types/camp.ts
@@ -0,0 +1,17 @@
+/*!
+ * Bloodline Blood Bank Management System
+ * Copyright (c) 2025 Onaliy Jayawardana
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
+export interface Camp {
+  organizationName: "";
+  fullName: "";
+  nic: "";
+  email: "";
+  contactNumber: "";
+  province: "";
+  district: "";
+  city: "";
+}
