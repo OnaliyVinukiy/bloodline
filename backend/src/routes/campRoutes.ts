4fb7d65d694f4e5945facb35cd3fa9f6dc5c20b6
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-16 13:17:16 +0530

    feat: Add route for camp registration

---
+++ backend/src/routes/campRoutes.ts
@@ -0,0 +1,15 @@
+/*!
+ * Bloodline Blood Bank Management System
+ * Copyright (c) 2025 Onaliy Jayawardana
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
+import express from "express";
+import { saveCamp } from "../controllers/campController";
+
+const router = express.Router();
+
+router.post("/register", saveCamp);
+
+export default router;
