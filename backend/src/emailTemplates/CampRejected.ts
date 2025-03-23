69bec3c9fa94fcd37de9322026917b61d27d7c77
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-03-23 20:35:32 +0530

    feat: Implement template for email rejection

---
+++ backend/src/emailTemplates/CampRejected.ts
@@ -0,0 +1,28 @@
+/*!
+ * Bloodline Blood Bank Management System
+ * Copyright (c) 2025 Onaliy Jayawardana
+ * All rights reserved.
+ *
+ * Unauthorized copying, modification, or distribution of this code is prohibited.
+ */
+export const CampRejection = (camp: any) => {
+  return `
+        <div style="font-family: Arial, sans-serif; background-color: #f4f4f9; padding: 20px; border-radius: 10px; max-width: 600px; margin: 0 auto;">
+          <div style="background-color: #e2e8f0; padding: 10px; border-radius: 5px; text-align: center;">
+            <h2 style="color: #2d3748;">Blood Donation Camp Rejection Notification</h2>
+          </div>
+          <div style="margin-top: 20px;">
+            <p style="font-size: 16px; color: #4a5568;">Dear ${camp.fullName},</p>
+            <p style="font-size: 16px; color: #4a5568;">We regret to inform you that your blood donation camp request has been rejected by the NBTS Team.</p>
+            <p style="font-size: 16px; color: #4a5568;"><strong>Camp Date:</strong> ${camp.date}</p>
+            <p style="font-size: 16px; color: #4a5568;"><strong>Status:</strong> ${camp.status}</p>
+             <p style="font-size: 16px; color: #4a5568;"><strong>Rejection Reason:</strong> ${camp.reason}</p>
+            <p style="font-size: 16px; color: #4a5568;">We appreciate your willingness to contribute, and thank you for your understanding.</p>
+          </div>
+          <div style="margin-top: 30px; text-align: center; font-size: 16px; color: #4a5568;">
+            <p>Best regards,</p>
+            <p><strong>NBTS</strong></p>
+          </div>
+        </div>
+      `;
+};
