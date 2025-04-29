d0686944cc2b8ae8bc4195cac628ada6d964469b
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-04-29 22:59:16 +0530

    refactor: Remove approve, reject icons and add donate icon

--- frontend/src/pages/admin/appointment/ApprovedAppointments.tsx
+++ frontend/src/pages/admin/appointment/ApprovedAppointments.tsx
@@ -198,54 +198,39 @@ const ApprovedAppointments = () => {
                         </svg>
                       </button>
                     </Link>
-                    <Link to={`/appointment/${appointment._id}`}>
-                      <button
-                        className="font-medium text-green-600 dark:text-green-500 hover:underline"
-                        aria-label="Approve"
-                      >
-                        <svg
-                          className="w-6 h-6 text-green-600 dark:text-white"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          fill="none"
-                          viewBox="0 0 24 24"
+                    <Link to={`/admin/donation/${appointment._id}`}>
+                        <button
+                          className="font-medium text-green-600 dark:text-green-500 hover:underline"
+                          aria-label="Donate"
+                          disabled={
+                            appointment.status === "Pending" ||
+                            appointment.status === "Rejected"
+                          }
                         >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M5 11.917L9.724 16.5 19 7.5"
-                          />
-                        </svg>
-                      </button>
-                    </Link>
-                    <Link to={`/appointment/${appointment._id}`}>
-                      <button
-                        className="font-medium text-red-600 dark:text-red-500 hover:underline"
-                        aria-label="Reject"
-                      >
-                        <svg
-                          className="w-6 h-6 text-red-600 dark:text-white"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M6 18 17.94 6M18 18 6.06 6"
-                          />
-                        </svg>
-                      </button>
-                    </Link>
+                          <svg
+                            className={`w-5 h-6 ${
+                              appointment.status === "Pending" ||
+                              appointment.status === "Rejected"
+                                ? "text-gray-400 dark:text-gray-500"
+                                : "text-blue-800 dark:text-white"
+                            }`}
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="currentColor"
+                            viewBox="0 0 512 512"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M441 7l32 32 32 32c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-15-15L417.9 128l55 55c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-72-72L295 73c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l55 55L422.1 56 407 41c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0zM210.3 155.7l61.1-61.1c.3 .3 .6 .7 1 1l16 16 56 56 56 56 16 16c.3 .3 .6 .6 1 1l-191 191c-10.5 10.5-24.7 16.4-39.6 16.4H97.9L41 505c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l57-57V325.3c0-14.9 5.9-29.1 16.4-39.6l43.3-43.3 57 57c6.2 6.2 16.4 6.2 22.6 0s6.2-16.4 0-22.6l-57-57 41.4-41.4 57 57c6.2 6.2 16.4 6.2 22.6 0s6.2-16.4 0-22.6l-57-57z"
+                            />
+                          </svg>
+                        </button>
+                      </Link>
                   </div>
                 </td>
 
