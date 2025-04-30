901b18a10cbeeb21c9ce8e3b85a4ef2e70ee1283
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2025-04-30 12:26:12 +0530

    refactor: Simplify Vite configuration by removing environment variable loading

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react()],
-   
-    define: {
-      'process.env.VITE_GOOGLE_MAPS_API_KEY': JSON.stringify(env.VITE_GOOGLE_MAPS_API_KEY),
-      'process.env.VITE_BACKEND_URL': JSON.stringify(env.VITE_BACKEND_URL),
-      'process.env.VITE_IS_PRODUCTION': JSON.stringify(env.VITE_IS_PRODUCTION)
-    },
-   
-    build: {
-      outDir: 'dist',
-      emptyOutDir: true,
-      sourcemap: mode !== 'production'
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
 })
